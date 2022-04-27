bfd67ef88525632cf5e017d4e46c720c18b3a741
Juan Martinez <cj5895576@gmail.com>
2022-04-27 20:44:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/semana13_sqlite/LoginRepuesto.java
@@ -0,0 +1,14 @@
+package com.example.semana13_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginRepuesto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_repuesto);
+    }
+}
